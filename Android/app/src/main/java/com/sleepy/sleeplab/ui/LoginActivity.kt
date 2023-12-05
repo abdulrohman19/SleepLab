29dc88570aa5e82a110e19ea0996b06d6d8cf948
abdul rohman <abdulrohmanunida@gmail.com>
2023-12-06 03:51:13 +0700

    test delete comment

--- Android/app/src/main/java/com/sleepy/sleeplab/ui/LoginActivity.kt
+++ Android/app/src/main/java/com/sleepy/sleeplab/ui/LoginActivity.kt
@@ -8,6 +8,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 
 }
+//cek
