a29d041c867a9348c26ba133f89acd3e57485512
abdul rohman <abdulrohmanunida@gmail.com>
2023-12-20 18:34:55 +0700

    Add Sleep Quality Activity

---
+++ Android/app/src/main/java/com/sleepy/sleeplab/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.sleepy.sleeplab.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sleepy.sleeplab.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
---
+++ Android/app/src/main/java/com/sleepy/sleeplab/ui/sleepQuality/SleepQualityActivity.kt
@@ -0,0 +1,12 @@
+package com.sleepy.sleeplab.ui.sleepQuality
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sleepy.sleeplab.R
+
+class SleepQualityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sleep_quality)
+    }
+}
