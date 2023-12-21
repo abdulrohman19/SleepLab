41e7e8333f029d4099aa5fe58450fafc50aaa627
abdul rohman <abdulrohmanunida@gmail.com>
2023-12-21 18:22:25 +0700

    Add Activity User Detail

---
+++ Android/app/src/main/java/com/sleepy/sleeplab/ui/profile/UserDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.sleepy.sleeplab.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sleepy.sleeplab.R
+
+class UserDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_detail)
+    }
+}
